f3b29894541edd3136d972948dfe9ae5c484a954
Chiruahs <chiruhas.bobbadi123@gmail.com>
2020-06-28 11:40:11 +0530

    zerodha futures fix

--- functions/util/alice/controller.js
+++ functions/util/alice/controller.js
@@ -74,7 +74,7 @@ module.exports.commodity = () => {
 
 };
 
-module.exports.commodity();
+
 
 module.exports.equity = () => {
 
--- functions/util/zerodha/future.js
+++ functions/util/zerodha/future.js
@@ -6,7 +6,7 @@ const path = require('path');
 try {
     function kite_call() {
         let kite_json = "";
-        let new_json = [];
+        let map = new Map();
         request.get("https://api.kite.trade/margins/futures", (error, response, body) => {
 
 
@@ -15,16 +15,44 @@ try {
                     body = body.trim();
                     kite_json = (JSON.parse(body));
 
-                    new_json = kite_json.map(element => {
-                        return {
-                            co_lower: element.co_lower,
-                            co_upper: element.co_upper,
-                            margin: element.margin,
-                            mis: element.mis_margin
+                    kite_json.map(element => {
+                        // return {
+                        //     co_lower: element.co_lower,
+                        //     co_upper: element.co_upper,
+                        //     margin: element.margin,
+                        //     mis: element.mis_margin,
+                        //     tradingsymbol:element.tradingsymbol
+                        // }
+
+                        let index = 0;
+                        for (let i = 0; i < element.tradingsymbol.length; i++) {
+                            if (isCharDigit(element.tradingsymbol.charAt(i))) {
+                                index = i;
+                                break;
+                            }
                         }
+
+                        function isCharDigit(n) {
+                            return !!n.trim() && n > -1;
+                        }
+
+                        let o = {
+                            co_lower: element.co_lower,
+                            co_upper: element.co_upper
+                        };
+
+                        if (!map.has(
+                            element.tradingsymbol.substr(0, index),
+                        ))
+                            map.set(element.tradingsymbol.substr(0, index), o);
+
                     });
-                    //console.log(new_json)
-                    call(new_json)
+                    const avg_co_lower = kite_json.reduce((sum, json) => sum + json.co_lower, 0) / kite_json.length;
+                    const avg_co_upper = kite_json.reduce((sum, json) => sum + json.co_upper, 0) / kite_json.length;
+
+                    //console.log(avg_co_upper.toFixed(2));
+
+                    call(map, avg_co_lower.toFixed(2), avg_co_upper.toFixed(2));
                 } catch (e) {
                     console.log("Exception in zerodha futures");
                 }
@@ -36,13 +64,13 @@ try {
 
         });
 
+
     }
 
-    function call(json) {
+    function call(map, lower, upper) {
 
 
-
-        let new_json = 0;
+        let new_json = [];
 
 
         request('https://zerodha.com/margin-calculator/Futures/', (error, response, html) => {
@@ -52,19 +80,30 @@ try {
 
                 const $ = cheerio.load(html);
 
-                new_json = json.map((obj, index) =>({
+                const calculateMis = (nrml, mis) => Math.floor((parseFloat(nrml) * (100 - parseFloat(mis))) / 100);
+
+
+                $('#header-container table tbody tr').each((index, el) => {
+
+
+                    const coLower = scrip => map.has(scrip) ? map.get(scrip).co_lower : lower;
+                    const coUpper = scrip => map.has(scrip) ? map.get(scrip).co_upper : upper;
+
+                    new_json.push({
                         scrip: $(`#entry-${index + 1}`).data('scrip'),
                         expiry: $(`#entry-${index + 1}`).data('expiry'),
                         lot: $(`#entry-${index + 1}`).data('lot_size'),
                         price: $(`#entry-${index + 1}`).data('price'),
                         nrml: $(`#entry-${index + 1}`).data('nrml_margin'),
-                        mis: Math.round(obj.mis),
-                        co_lower: obj.co_lower,
-                        co_upper: obj.co_upper,
-                        margin: obj.margin,
-                        mis_multiplier: obj.mis_multiplier
-                    })
-                );
+                        mis: calculateMis($(`#entry-${index + 1}`).data('nrml_margin'), $(el).data('margin')),
+                        co_lower: coLower($(`#entry-${index + 1}`).data('scrip')),
+                        co_upper: coUpper($(`#entry-${index + 1}`).data('scrip'))
+
+
+                    });
+
+
+                });
 
                 new_json = JSON.stringify(new_json);
 
@@ -74,7 +113,6 @@ try {
                     else
                         console.log("zerodha Future file created\n" + new Date())
                 })
-
 
             }
 
@@ -87,7 +125,6 @@ try {
 } catch (e) {
     console.log("exception in zerodha futures" + new Date().getDate());
 }
-
 
 exports.call = kite_call;
 
